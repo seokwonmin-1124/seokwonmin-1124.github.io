60f7d3adc682c995431e3dbd99b1a5f1ec17384e
seokwonmin-1124 <seokwonmin@naver.com>
2022-02-20 21:00:28 +0900

    최종_진짜_최종

--- leditor/leditor.js
+++ leditor/leditor.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
     $('#htmlView').hide();
 });
+
 function convertToHTML() {
     $('#htmlView').text($('#textView').html());
     $('#htmlView').show();
     $('#textView').hide();
 }
+
 function convertToText() {
     $('#textView').html($('#htmlView').text());
     $('#textView').show();
